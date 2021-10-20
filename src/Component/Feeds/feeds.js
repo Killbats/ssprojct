5316f2ff47c4ee55c183d37033e951d0fe451f7d
Killbats <killbats@users.noreply.github.com>
2021-10-20 17:01:02 +0700

    fix files

--- src/Component/Feeds/feeds.js
+++ src/Component/Feeds/feeds.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import Card from 'react-bootstrap/Card';
-import misk from '../../dummyimg/Tobisawa.Misaki.full.1809497.jpg';
+import misk from '../../dummyimg/2559.jpg';
+import Post from "../Post/post";
 import './feeds.css';
 
-function feeds(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+function Feeds(){
+
+    const [state, setData] = useState([]);
+
+    const apiGet = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+        setData(json);
+        });
+};
+    useData(() =>{
+        api.get();
+    },[])
+
     return(
+
     <div className="midBar">
+        <Post/>
         <Card className="cardTable">
             <div className="txtpad"> Poster Name</div>
-                <Card.Img className="feedsCard" variant="top" src={misk} />
+            <a href={misk}>
+                <Card.Img className="feedsCard" variant="top" src={misk}/>
             <div className="txtpad">Comment</div>
+            </a>
         </Card>
+        {data.map((item) => (<h1 key={item.id}>{item.title} </h1>))}
     </div>
     );
 };
 
-export default feeds;
+export default Feeds;
---
+++ src/Component/Post/post.js
@@ -0,0 +1,18 @@
+import {Card} from 'react-bootstrap';
+import './post.css';
+
+export default function post(){
+    return(
+        <div className="postMarg">
+            <Card className="cardPost">
+            <div>
+                <input className="postInpt" placeholder="What's in your mind ?"/>
+            </div>
+            <div >
+                <button className="postBtn" type="button">Post</button>
+            </div>
+            </Card>
+            <br/>
+        </div>
+    );
+}
